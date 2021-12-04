c5162a5650f9b268349b0e407b19fc327d72baa5
SelimMeske <mesic.selim@outlook.com>
2021-12-04 16:43:16 +0100

    Last Seven

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const moment = require("moment");
 
 const app = express();
 const router = express.Router();
@@ -17,9 +18,7 @@ router.get("/get-last-seven-days", (req, res) => {
     d.setDate(d.getDate() - i)
     return moment(d).format('YYYYMMDD')
   })
-  res.json(
-    {"lastSevenDays": dates}
-    );
+  res.json({"lastSevenDays": dates});
 });
 
 app.use(`/.netlify/functions/api`, router);
